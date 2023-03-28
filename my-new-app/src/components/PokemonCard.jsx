508c676966276f633607f3f84d98ec6196b560cd
hugocoding21 <hhanocq@gmail.com>
2023-03-28 15:52:21 +0200

    quete3

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,9 +1,26 @@
-export default function PokemonCard(){ return (
-<figure>
-  <img
-    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    alt="bulbasaur"
-  />
-  <figcaption>bulbasaur</figcaption>
-</figure>
-) }
+export default function PokemonCard() {
+  const pokemon = pokemonList[0];
+
+  console.log(pokemon);
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt="bulbasaur" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
